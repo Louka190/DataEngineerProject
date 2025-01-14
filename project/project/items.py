71aceb8cb9270bbb2154ae16c1c1eadb39dd71c0
lecoinn <nathan.lecoin@edu.esiee.fr>
2025-01-14 16:18:43 +0100

    classe ArticleItem

--- project/project/items.py
+++ project/project/items.py
@@ -6,7 +6,25 @@
 import scrapy
 
 
-class ProjectItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ArticletItem(scrapy.Item):
+    
+    """    
+    Définition des champs de l'item pour Scrapy
+    Représente les données extraites du tableau des nageurs de 100m freestyle du site Olympedia 
+    Pos = Position du nageur
+    Swimmer = Nom du nageur
+    NOC = Nationalité du nageur
+    R1 = Temps de la première course
+    SF = Temps de la demi-finale
+    Final = Temps de la finale
+    Medal = Médaille gagnée
+    """    
+    
+    Pos	= scrapy.Field()
+    Swimmer = scrapy.Field()
+    NOC	= scrapy.Field()
+    R1 = scrapy.Field()
+    SF = scrapy.Field()	
+    Final = scrapy.Field()
+    Medal = scrapy.Field()
+
